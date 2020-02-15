319df576a1291635577582c6a3dbd2ef0ff57dfd
Matt Charlton <Matt.charlton@noredink.com>
2020-02-15 17:11:23 +0000

    animate on keydown

--- Day 1 - JS Drum Kit/script.js
+++ Day 1 - JS Drum Kit/script.js
@@ -1,7 +1,13 @@
 document.addEventListener('keydown', event => {
   const sound = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`)
   if (sound !== null) {
     sound.currentTime = 0;
     sound.play();
+
+    key.classList.add('playing')
+    setTimeout(function(){
+        key.classList.remove('playing');
+    }, 200);
   }
 })
